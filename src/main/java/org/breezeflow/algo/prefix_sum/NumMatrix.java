85d8593194b8baaadcb7c796c3983eee178a8684
chenh <chenh@4px.com>
2023-02-08 11:11:44 +0800

    algo:前缀和数组

--- src/main/java/org/breezeflow/algo/prefix_sum/NumMatrix.java
+++ src/main/java/org/breezeflow/algo/prefix_sum/NumMatrix.java
@@ -12,12 +12,27 @@ public class NumMatrix {
     private int[][] preSum;
 
     public NumMatrix(int[][] matrix) {
-
+        int m = matrix.length, n = matrix[0].length;
+        if (n == 0) {
+            return;
+        }
+        preSum = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                preSum[i][j] = preSum[i - 1][j] + preSum[i][j - 1] + matrix[i - 1][j - 1] - preSum[i - 1][j - 1];
+            }
+        }
     }
 
-    public int sumRegion(int row1, int col1, int row2, int col2) {
+    public int sumRegion(int x1, int y1, int x2, int y2) {
+        return preSum[x2 + 1][y2 + 1] - preSum[x1][y2 + 1] - preSum[x2 + 1][y1] + preSum[x1][y1];
+    }
 
-        return 0;
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{3, 0, 1, 4, 2}, {5, 6, 3, 2, 1}, {1, 2, 0, 1, 5}, {4, 1, 0, 1, 7}, {1, 0, 3, 0, 5}};
+        NumMatrix demo = new NumMatrix(matrix);
+
+        System.out.println(demo.sumRegion(1, 1, 3, 3));
     }
 
 }
---
+++ src/main/java/org/breezeflow/algo/prefix_sum/SubArraySum.java
@@ -0,0 +1,40 @@
+package org.breezeflow.algo.prefix_sum;
+
+/**
+ * 算法技巧(前缀和)训练案例:和为 K 的子数组
+ *
+ * @author breezeflow
+ * @link https://leetcode.cn/problems/subarray-sum-equals-k/
+ * @date 2023/02/08
+ */
+public class SubArraySum {
+
+    int[] preSum;
+
+    public int subarraySum(int[] nums, int k) {
+
+        //构造前缀和数组
+        preSum = new int[nums.length + 1];
+        for (int i = 1; i < preSum.length; i++) {
+            preSum[i] = preSum[i - 1] + nums[i - 1];
+        }
+
+        int count = 0;
+        // 计算子串区间和是否等于k
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (preSum[j + 1] - preSum[i] == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        SubArraySum demo = new SubArraySum();
+        int[] num = new int[]{1, 2, 3};
+        System.out.println(demo.subarraySum(num, 3));
+    }
+
+}
