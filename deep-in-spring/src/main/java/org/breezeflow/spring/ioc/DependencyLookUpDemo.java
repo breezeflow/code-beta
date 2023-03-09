9fba3e0722e2cd9b89b63de93c0d773e7299ef1f
breezeflow <chenhh309@gmail.com>
2023-03-09 23:23:40 +0800

    update

--- deep-in-spring/src/main/java/org/breezeflow/spring/ioc/DependencyLookUpDemo.java
+++ deep-in-spring/src/main/java/org/breezeflow/spring/ioc/DependencyLookUpDemo.java
@@ -1,6 +1,8 @@
 package org.breezeflow.spring.ioc;
 
+import org.breezeflow.spring.domain.User;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -14,6 +16,22 @@ public class DependencyLookUpDemo {
     public static void main(String[] args) {
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("META-INF/dependency-lookup.xml");
 
-        System.out.println(beanFactory.getBean("user"));
+        //实时查找
+        lookUpInRealTime(beanFactory);
+
+        //延时查找
+        lookUpInLazy(beanFactory);
+
+    }
+
+    private static void lookUpInRealTime(BeanFactory beanFactory) {
+        User user = (User) beanFactory.getBean("user");
+        System.out.println("实时查找" + user);
+    }
+
+    private static void lookUpInLazy(BeanFactory beanFactory) {
+        ObjectFactory<User> objectFactory = (ObjectFactory<User>) beanFactory.getBean("objectFactory");
+        User user = objectFactory.getObject();
+        System.out.println("延时查找" + user);
     }
 }
