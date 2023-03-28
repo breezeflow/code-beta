d16bbb87c9d7c69168275c9210d754579e4dd025
breezeflow <chenhh309@gmail.com>
2023-03-29 00:23:54 +0800

    update

---
+++ base-tech/src/main/java/org/breezeflow/base/tech/IntegerExample.java
@@ -0,0 +1,27 @@
+package org.breezeflow.base.tech;
+
+/**
+ * @author andy
+ * @version 2023/03/28
+ */
+public class IntegerExample {
+
+    public static void main(String[] args) {
+        Integer a = 123;
+        Integer b = 123;
+        Integer x = new Integer(123);
+        Integer y = new Integer(123);
+        Integer m = Integer.valueOf(123);
+        Integer n = Integer.valueOf(123);
+        Integer r = 128;
+        Integer s = Integer.valueOf(128);
+
+        System.out.println(a == b); // true
+        System.out.println(a == x); // false
+        System.out.println(a == m); // true
+        System.out.println(x == y); // false
+        System.out.println(x == m); // false
+        System.out.println(m == n); // true
+        System.out.println(r == s); // false
+    }
+}
